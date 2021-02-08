b6d20d6404e8602326e7339f81cdb70f8aa79b5d
24kawerka <qweqwerka@gmail.com>
2021-02-08 19:35:24 +0200

    commit4

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: "https://24kawerka.github.io",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 });
